4dbf7854d923363f4229d94eec1963ad185ebf5c
rezwanahammad <127617637+rezwanahammad@users.noreply.github.com>
2025-05-17 20:27:11 +0600

    c1

--- src/app/api/districts-by-slug/[slug]/route.ts
+++ src/app/api/districts-by-slug/[slug]/route.ts
@@ -1,16 +1,12 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "../../../../../lib/mongodb";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { slug: string } }
+  { params }: { params: { slug: string } }
 ) {
-  const { slug } = context.params;
-
   const { db } = await connectToDatabase();
-  const district = await db.collection("districts").findOne({ slug });
+  const district = await db.collection("districts").findOne({ slug: params.slug });
 
   if (!district) {
     return NextResponse.json({ message: "District not found" }, { status: 404 });
